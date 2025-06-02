dc6e081909dc468caaf9639f9b90f5f498adb8a7
jorge <jorgemartinezpizarro@gmail.com>
2025-06-02 15:31:24 +0200

    use gmp to compute big integers in C

--- c/lucas_lehmer.c
+++ c/lucas_lehmer.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <math.h>
+#include <gmp.h>
 #include <string.h>
 #include <stdbool.h>
-#include <unistd.h>
-
-#define MAX_NUMBERS 100
 
 typedef struct {
     int p;
@@ -18,39 +15,53 @@ typedef struct {
     int index;
 } ThreadInput;
 
-TestResult results[MAX_NUMBERS];
-
-bool is_mersenne_prime(int p) {
-    if (p == 2) return true;
-    long long m = (1LL << p) - 1;
-    long long s = 4;
-    for (int i = 0; i < p - 2; ++i) {
-        s = ((s * s) - 2) % m;
-    }
-    return s == 0;
-}
+TestResult* results;
+pthread_t* threads;
 
 void* thread_func(void* arg) {
     ThreadInput* input = (ThreadInput*)arg;
-    results[input->index].p = input->p;
-    results[input->index].result = is_mersenne_prime(input->p);
+    int p = input->p;
+    int index = input->index;
+
+    mpz_t m, s, two;
+    mpz_inits(m, s, two, NULL);
+
+    // m = 2^p - 1
+    mpz_ui_pow_ui(m, 2, p);
+    mpz_sub_ui(m, m, 1);
+
+    mpz_set_ui(s, 4);
+    mpz_set_ui(two, 2);
+
+    for (int i = 0; i < p - 2; ++i) {
+        mpz_mul(s, s, s);
+        mpz_sub(s, s, two);
+        mpz_mod(s, s, m);
+    }
+
+    results[index].p = p;
+    results[index].result = mpz_cmp_ui(s, 0) == 0;
+
+    mpz_clears(m, s, two, NULL);
     free(arg);
     return NULL;
 }
 
 int main() {
-    char buffer[4096];
+    char buffer[8192];
     fread(buffer, 1, sizeof(buffer), stdin);
 
-    // Esperamos un JSON como [3, 5, 7]
-    int numbers[MAX_NUMBERS], count = 0;
+    int* numbers = malloc(sizeof(int) * 10000);
+    int count = 0;
     char* token = strtok(buffer, "[,]");
-    while (token && count < MAX_NUMBERS) {
+    while (token) {
         numbers[count++] = atoi(token);
         token = strtok(NULL, "[,]");
     }
 
-    pthread_t threads[MAX_NUMBERS];
+    results = malloc(sizeof(TestResult) * count);
+    threads = malloc(sizeof(pthread_t) * count);
+
     for (int i = 0; i < count; ++i) {
         ThreadInput* input = malloc(sizeof(ThreadInput));
         input->p = numbers[i];
@@ -68,5 +79,8 @@ int main() {
     }
     printf("]\n");
 
+    free(numbers);
+    free(results);
+    free(threads);
     return 0;
 }
